4584425fab5797ce0decdf76b4eb60f38e2cd5bd
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2024-01-25 19:21:04 +0100

    downgraded mixins to 7.11-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import dev.deftu.gradle.utils.GameSide
 plugins {
     java
     kotlin("jvm") version("1.9.21")
-    val dgtVersion = "1.22.0"
+    val dgtVersion = "1.22.4"
     id("dev.deftu.gradle.tools") version(dgtVersion)
     id("dev.deftu.gradle.tools.kotlin") version(dgtVersion)
     id("dev.deftu.gradle.tools.shadow") version(dgtVersion)
@@ -12,24 +12,17 @@ plugins {
     id("dev.deftu.gradle.tools.resources") version(dgtVersion)
 }
 
-loom {
-    forge {
-        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-    }
-}
-
 toolkit.useDevAuth()
 
 toolkitLoomHelper  {
     useForgeMixin(modData.id)
-    useMixinRefMap(modData.id)
     useTweaker("org.spongepowered.asm.launch.MixinTweaker")
     useProperty("mixin.debug.export", "true", GameSide.CLIENT)
     disableRunConfigs(GameSide.SERVER)
 }
 
 repositories {
-    maven("https://maven.fabricmc.net/")
+    maven("https://repo.spongepowered.org/maven/")
     maven("https://maven.deftu.dev/releases")
     mavenCentral()
 }
@@ -37,10 +30,7 @@ repositories {
 dependencies {
     implementation(shade(kotlin("stdlib"))!!)
 
-    implementation(shade("net.fabricmc:sponge-mixin:0.12.5+mixin.0.8.5") {
-        exclude(module = "launchwrapper")
-        exclude(module = "guava")
-        exclude(module = "gson")
-        exclude(module = "commons-io")
+    implementation(shade("org.spongepowered:mixin:0.7.11-SNAPSHOT") {
+        isTransitive = false
     })
 }
